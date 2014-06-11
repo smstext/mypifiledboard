502f4df462334340cb8379819b79da2d7d71c658
ZacharyIgielman <zacharyi123@me.com>
2014-06-11 10:16:27 +0100

    Create lightLEDs.py

---
+++ lightLEDs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# lightLEDs.py
+# Button press loops through LEDs like this (0 is led off, 1 is led on): 000, 100, 110, 111, 011, 001, 000, 100, 110, 111, 011, 001, 000 etc.
+# Author : Zachary Igielman
+
+import RPi.GPIO as GPIO, time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(7, GPIO.OUT) #red
+GPIO.setup(15, GPIO.OUT) #amber
+GPIO.setup(21, GPIO.OUT) #green
+GPIO.setup(26, GPIO.IN) #button
+
+GPIO.output(7, GPIO.LOW)
+GPIO.output(15, GPIO.LOW)
+GPIO.output(21, GPIO.LOW)
+
+def setLEDs(tempArray):
+  GPIO.output(7, tempArray[0])
+  GPIO.output(15, tempArray[1])
+  GPIO.output(21, tempArray[2])
+  
+array=[[0,0,0],[1,0,0],[1,1,0],[1,1,1],[0,1,1],[0,0,1]]
+
+a=0
+
+while True:
+  while GPIO.input(26)==1:
+    time.sleep(0.1)
+  setLEDs(a%6)
+  a=a+1
+  while GPIO.input(26)==0:
+    time.sleep(0.1)
