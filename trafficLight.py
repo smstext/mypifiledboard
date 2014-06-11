988d333ab77936f1c28f89019127aaff9f4d4d41
ZacharyIgielman <zacharyi123@me.com>
2014-06-11 10:04:20 +0100

    Create trafficLight.py

---
+++ trafficLight.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# trafficLight.py
+# loops through red, yellow, green like a traffic light
+# Author : Zachary Igielman
+
+import RPi.GPIO as GPIO, time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(7, GPIO.OUT) #red
+GPIO.setup(15, GPIO.OUT) #amber
+GPIO.setup(21, GPIO.OUT) #green
+
+GPIO.output(7, GPIO.LOW)
+GPIO.output(15, GPIO.LOW)
+GPIO.output(21, GPIO.LOW)
+
+def setLEDs(red,amber,green):
+  GPIO.output(7, red)
+  GPIO.output(15, amber)
+  GPIO.output(21, green)
+
+while True:
+  setLEDs(1,0,0)
+  time.sleep(5)
+  setLEDs(1,1,0)
+  time.sleep(2)
+  setLEDs(0,0,1)
+  time.sleep(10)
+  setLEDs(0,1,0)
+  time.sleep(2)
