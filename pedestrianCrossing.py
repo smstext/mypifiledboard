4ea60470d89dc04337f20111e1333791e033d9ae
ZacharyIgielman <zacharyi123@me.com>
2014-07-19 11:53:39 +0100

    Update pedestrianCrossing.py

--- pedestrianCrossing.py
+++ pedestrianCrossing.py
@@ -8,17 +8,17 @@ import RPi.GPIO as GPIO, time, sys
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.OUT) #red
-GPIO.setup(15, GPIO.OUT) #amber
+GPIO.setup(13, GPIO.OUT) #amber
 GPIO.setup(21, GPIO.OUT) #green
 GPIO.setup(26, GPIO.IN) #button
 
-GPIO.output(7, GPIO.LOW)
-GPIO.output(15, GPIO.LOW)
+GPIO.output(11, GPIO.LOW)
+GPIO.output(13, GPIO.LOW)
 GPIO.output(21, GPIO.LOW)
 
 def setLEDs(red,amber,green):
-  GPIO.output(7, red)
-  GPIO.output(15, amber)
+  GPIO.output(11, red)
+  GPIO.output(13, amber)
   GPIO.output(21, green)
 
 try:
